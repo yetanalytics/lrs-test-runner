e1ebc0bce86ee076cb630a3c77122a61adde993e
Milt Reder <milt@yetanalytics.com>
2021-05-21 15:45:08 -0400

    just make then an array of lines

--- src/main/com/yetanalytics/lrs/test_runner.clj
+++ src/main/com/yetanalytics/lrs/test_runner.clj
@@ -4,7 +4,8 @@
             [clojure.pprint :as pprint]
             [clojure.walk :as w]
             [clojure.java.io :as io]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+            [clojure.string :as cs])
   (:import [java.io File]
            [java.nio.file.attribute FileAttribute]
            [java.nio.file Files Path]
@@ -94,14 +95,11 @@
                         {:type ::delete-lockfile-exception
                          :tempdir tempdir})))))
 
-
-(defrecord RequestLog [out-str])
-
 (defn wrap-request-logs
   [log-root]
   (w/prewalk (fn [node]
                (if (some-> node :log string?)
-                 (update node :log #(RequestLog. %))
+                 (update node :log cs/split-lines)
                  node))
              log-root))
 
